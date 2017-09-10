b4b92b3b947dec39f9bf7f1f365a46dd3c98e73f
regalius <regalius.contact@gmail.com>
2017-09-10 12:01:28 +0700

    Pokeapi endpoint

--- src/constants/URL.js
+++ src/constants/URL.js
@@ -1,5 +1,8 @@
-export const POKE_API_ENDPOINT ="http://pokeapi.salestock.net:8000/api/v2/";
+export const POKE_API_ENDPOINT = "http://pokeapi.co/api/v2/pokemon/";
 export const POKEMON_IMAGE_URL = "https://img.pokemondb.net/artwork/";
-export const POKEMON_IMAGE_NODATA_URL = "https://img.pokemondb.net/artwork/bulbasaur.jpg";
-export const POKEMON_SPRITES_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-export const POKEMON_SPRITES_NODATA_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+export const POKEMON_IMAGE_NODATA_URL =
+  "https://img.pokemondb.net/artwork/bulbasaur.jpg";
+export const POKEMON_SPRITES_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+export const POKEMON_SPRITES_NODATA_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
