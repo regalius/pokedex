41a17959d26b041b9cdaf222755fd03e7c0e234c
Felix Tan <regalius.contact@gmail.com>
2018-05-06 18:25:59 +0700

    Update URL

--- src/constants/URL.js
+++ src/constants/URL.js
@@ -1,8 +1,8 @@
-export const POKE_API_ENDPOINT = "http://pokeapi.co/api/v2/";
+export const POKE_API_ENDPOINT = "http://pokeapi.salestock.net/api/v2/";
 export const POKEMON_IMAGE_URL = "https://img.pokemondb.net/artwork/";
 export const POKEMON_IMAGE_NODATA_URL =
-  "https://img.pokemondb.net/artwork/bulbasaur.jpg";
+    "https://img.pokemondb.net/artwork/bulbasaur.jpg";
 export const POKEMON_SPRITES_URL =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 export const POKEMON_SPRITES_NODATA_URL =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
