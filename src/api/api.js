c93a240f538379ba641b39a9240b85c28d2baea6
Felix Tan <regalius.contact@gmail.com>
2018-05-06 18:25:59 +0700

    Update URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 export const PokeFetch = url => {
-  return fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(json => {
-      return json;
-    });
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            return json;
+        });
 };
